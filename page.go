b5c1676166f8ac7ae59726ff2f0fd2e2cb37f49d
billcoding <wangmiao07@sina.com>
2021-01-23 22:46:48 +0800

    Add page map

--- page.go
+++ page.go
@@ -1,9 +1,17 @@
 package gobatis
 
-type page struct {
+type Page struct {
 	Offset    int           `json:"offset"`
 	Size      int           `json:"size"`
 	TotalRows int           `json:"total_rows"`
 	TotalPage int           `json:"total_page"`
 	List      []interface{} `json:"list"`
+}
+
+type PageMap struct {
+	Offset    int                      `json:"offset"`
+	Size      int                      `json:"size"`
+	TotalRows int                      `json:"total_rows"`
+	TotalPage int                      `json:"total_page"`
+	List      []map[string]interface{} `json:"list"`
 }
--- select_page.go
+++ select_page.go
@@ -5,18 +5,18 @@ import (
 	"math"
 )
 
-// Page select page
-func (m *SelectMapper) Page(rptr interface{}, offset, size int, args ...interface{}) *page {
+// Page select Page
+func (m *SelectMapper) Page(rptr interface{}, offset, size int, args ...interface{}) *Page {
 	return m.PageWithParamsArgs(rptr, offset, size, nil, args...)
 }
 
 // PageWithParams select exec with params
-func (m *SelectMapper) PageWithParams(rptr interface{}, offset, size int, params ...*Param) *page {
+func (m *SelectMapper) PageWithParams(rptr interface{}, offset, size int, params ...*Param) *Page {
 	return m.PageWithParamsArgs(rptr, offset, size, params)
 }
 
 // PageWithParamsArgs select exec with args and named params
-func (m *SelectMapper) PageWithParamsArgs(rptr interface{}, offset, size int, params []*Param, args ...interface{}) *page {
+func (m *SelectMapper) PageWithParamsArgs(rptr interface{}, offset, size int, params []*Param, args ...interface{}) *Page {
 	defer func() {
 		if re := recover(); re != nil {
 			m.logger.Error("%v", re)
@@ -31,7 +31,7 @@ func (m *SelectMapper) PageWithParamsArgs(rptr interface{}, offset, size int, pa
 		offset = 0
 	}
 
-	page := &page{
+	page := &Page{
 		Offset: offset,
 		Size:   size,
 		List:   []interface{}{},
@@ -55,6 +55,61 @@ func (m *SelectMapper) PageWithParamsArgs(rptr interface{}, offset, size int, pa
 		// valid range
 		m.extraSql = fmt.Sprintf(" limit %d,%d", offset, size)
 		page.List = m.Exec(args...).List(rptr)
+		m.extraSql = ""
+	}
+	return page
+}
+
+// PageMap select Page
+func (m *SelectMapper) PageMap(offset, size int, args ...interface{}) *PageMap {
+	return m.PageMapWithParamsArgs(offset, size, nil, args...)
+}
+
+// PageMapWithParams select exec with params
+func (m *SelectMapper) PageMapWithParams(offset, size int, params ...*Param) *PageMap {
+	return m.PageMapWithParamsArgs(offset, size, params)
+}
+
+// PageMapWithParamsArgs select exec with args and named params
+func (m *SelectMapper) PageMapWithParamsArgs(offset, size int, params []*Param, args ...interface{}) *PageMap {
+	defer func() {
+		if re := recover(); re != nil {
+			m.logger.Error("%v", re)
+		}
+	}()
+
+	if size <= 0 {
+		size = 10
+	}
+
+	if offset < 0 {
+		offset = 0
+	}
+
+	page := &PageMap{
+		Offset: offset,
+		Size:   size,
+		List:   []map[string]interface{}{},
+	}
+
+	if params != nil {
+		//replace named param
+		m.replaceParams(params...)
+	}
+
+	//First query total count
+	totalRows := m.queryCountByDB(args...)
+
+	if m.printSql {
+		m.logger.Info("binding[%s] selectPage[%s] exec : sql(%v), args(%v)", m.binding, m.id, m.sql, args)
+	}
+
+	if totalRows > 0 && offset >= 0 && offset <= totalRows-1 {
+		page.TotalRows = totalRows
+		page.TotalPage = int(math.Ceil(float64(totalRows) / float64(size)))
+		// valid range
+		m.extraSql = fmt.Sprintf(" limit %d,%d", offset, size)
+		page.List = m.Exec(args...).MapList()
 		m.extraSql = ""
 	}
 	return page
