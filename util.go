b660121b52054cfab1d36d05b776c89ce0f087f5
billcodng <wangmiao07@sina.com>
2021-03-17 08:59:47 +0800

    Remove  getMapperFiles

--- util.go
+++ util.go
@@ -1,34 +1,9 @@
 package gobatis
 
 import (
-	"io/ioutil"
-	"path/filepath"
-	"regexp"
 	"strings"
 	"text/template"
 )
-
-func getMapperFiles(dir ...string) []string {
-	var mapperFiles []string
-	for _, mapperPath := range dir {
-		fs, err := ioutil.ReadDir(mapperPath)
-		if err != nil {
-			continue
-		}
-		for _, f := range fs {
-			curr := mapperPath + string(filepath.Separator) + f.Name()
-			if f.IsDir() {
-				mapperFiles = append(mapperFiles, getMapperFiles(curr)...)
-				continue
-			}
-			match, err := regexp.MatchString("^.+\\.[xX][mM][lL]$", f.Name())
-			if err == nil && match {
-				mapperFiles = append(mapperFiles, curr)
-			}
-		}
-	}
-	return mapperFiles
-}
 
 func replaceParams(sql string, params ...*Param) string {
 	replacedSql := sql
